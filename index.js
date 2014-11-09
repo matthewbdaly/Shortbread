059a32e4aeb69617a17eac432fd2451282faf425
Matthew Daly <matthewbdaly@gmail.com>
2014-11-09 00:16:03 +0000

    Now works, but test does not pass

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 
 // Declare variables used
-var app, client, express, port, redis, shortid;
+var app, bodyParser, client, express, port, redis, shortid;
 
 // Define values
 express = require('express');
@@ -12,11 +12,18 @@ redis = require('redis');
 client = redis.createClient();
 client.select(1);
 shortid = require('shortid');
+bodyParser = require('body-parser');
 
 // Set up templating
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+
+// Handle POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Define index route
 app.get('/', function (req, res) {
@@ -29,7 +36,7 @@ app.post('/', function (req, res) {
     var url, id;
 
     // Get URL
-    url = req.param('url');
+    url = req.body.url;
 
     // Create a hashed short version
     id = shortid.generate();
@@ -38,6 +45,26 @@ app.post('/', function (req, res) {
     client.set(id, url, function () {
         // Display the response
         res.render('output', { id: id });
+    });
+});
+
+// Define link route
+app.route('/:id').all(function (req, res) {
+    // Get ID
+    var id = req.params.id.trim();
+
+    // Look up the URL
+    client.get(id, function (err, reply) {
+        if (!err) {
+            // Redirect user to it
+            res.status(301);
+            res.set('Location', reply);
+            res.send();
+        } else {
+            // Confirm no such link in database
+            res.status(404);
+            res.render('error');
+        }
     });
 });
 
